bfa62132d7305ccbf4f1bbd9c967056a5b80540c
saintaubins <saintaubins@gmail.com>
2020-09-13 16:12:17 -0400

    front and back working ready to deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
+const cors = require('cors');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,16 +13,47 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.use(cors());
+app.use(router);
 // this manages all the sockets
 io.on('connection', (socket) => {
-    console.log('We have a new connection.');
+    
+    socket.on('join', ({ name, room }, callback) => {
+        const {error, user } = addUser({ id: socket.id, name, room });
+
+        if(error) return callback(error);
+
+        socket.join(user.room);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        //getting message from the front end
+        io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+
+        callback();
+    });
 
     socket.on('disconnect', () => {
-        console.log('User has left.');
+        const user = removeUser(socket.id);
+        
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
+        console.log('User has left!!!');
     })
 });
 
-app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
